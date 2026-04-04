bf268e1777abc707f3d0f81cef14f40af9240974
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-04 15:05:46 +0530

    1-pract

---
+++ Practices/1-primitive-datatype.js
@@ -0,0 +1,29 @@
+//nnssbbu null number symbol string boolen biginit undefined
+
+let a=null;
+console.log(a);
+console.log(typeof a)
+
+let b=12;
+console.log(b);
+console.log(typeof b);
+
+let c=Symbol('1121');
+console.log(c);
+console.log(typeof c);
+
+let d="pavan"
+console.log(d);
+console.log(typeof d);
+
+let e=true;
+console.log(e);
+console.log(typeof e);
+
+let f=12131313n;
+console.log(f);
+console.log(typeof f);
+
+let g;
+console.log(g);
+console.log(typeof g);
