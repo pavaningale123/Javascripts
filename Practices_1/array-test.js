91cec5d8032b40a48865deb44f881e667621b368
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-07 12:30:38 +0530

    array-test

---
+++ Practices_1/array-test.js
@@ -0,0 +1,20 @@
+let arr=[12,23,34,"pavan",12.23,true]
+let arr1=[54,67]
+console.log(arr)
+console.log(typeof arr[5]);
+
+console.log(arr.indexOf(12))
+arr.push(1111)
+console.log(arr)
+arr.unshift(22)
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.shift()
+console.log(arr)
+const result= arr.concat(arr1)
+console.log(result)
+arr.reverse()
+console.log(arr)
+arr.sort()
+console.log(arr)
