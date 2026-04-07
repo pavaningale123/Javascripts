bdebf4d46ff32922d460e10b109105037aa934f3
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-07 12:57:39 +0530

    array-date

---
+++ Practices_1/array-date.js
@@ -0,0 +1,11 @@
+function dates(){
+    let date= new Date();
+    console.log(date);
+    console.log(date.getDate());
+    console.log(date.getDay());
+    console.log(date.getFullYear());
+    console.log(date.getTimezoneOffset());
+    console.log(date.setMinutes(0));
+    console.log(date);
+}
+dates()
