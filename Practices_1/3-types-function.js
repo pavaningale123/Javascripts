0acb795cac462c9149832cd372974382f1f09713
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-07 12:06:46 +0530

    3-types-function

---
+++ Practices_1/3-types-function.js
@@ -0,0 +1,22 @@
+//normal function
+
+function student(){
+    //let name="Pavan";
+    console.log("This is normal function ");
+}
+student();
+
+// parameterise function
+function add(a,b){
+    console.log(a+b);
+}
+add(4,4);
+
+//return function
+
+function addition(c,d){
+    return c+d;
+}
+
+let result=addition(5,5);
+console.log(result)
