437cc48ecc40c4dd8419c2f03921c2cfdcc86051
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-07 11:11:05 +0530

    practice-1-promitive-data-type-7-4-26

---
+++ Practices_1/primitive-data-type.js
@@ -0,0 +1,28 @@
+//nnssbbu - null number string symbol boolean biginit undefined
+let a=null;
+console.log(a);
+console.log(typeof a);
+
+let b=123.123//123 number;
+console.log(b);
+console.log(typeof b);
+
+let s='pavan';
+console.log(s);
+console.log(typeof s);
+
+let d=Symbol('id');
+console.log(d);
+console.log(typeof d);
+
+let e=1234524242424223424242n;
+console.log(e);
+console.log(typeof e);
+
+let f=true;
+console.log(f);
+console.log(typeof f);
+
+var g;
+console.log(g);
+console.log(typeof g);
