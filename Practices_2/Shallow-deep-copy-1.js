b8e823cd35c8756220ed23c6646b6c8fd2f28722
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-24 13:03:49 +0530

    change1

--- Practices_2/Shallow-deep-copy-1.js
+++ Practices_2/Shallow-deep-copy-1.js
@@ -6,12 +6,9 @@ const person={
 }
 
 //shallow copy
-
 // When using shallow copy (Object.assign), modifying the nested property (address.city) in the cloned object also changes the original object.
 // This happens because both objects share the same reference for the nested object.
-
 // 👉 That’s why output shows:
-
 // Original → "Mumbai"
 // Cloned → "Mumbai"
 
@@ -24,13 +21,9 @@ console.log("Original obejct:- ", person);
 console.log("Cloned obejct:- ", shallowobject);
 
 //deep copy
-
 // 👉 When using deep copy (JSON.parse(JSON.stringify())), modifying the cloned object does not affect the original object.
-
 // 👉 This is because deep copy creates a separate memory reference for nested objects.
-
 // 👉 That’s why output shows:
-
 // Original → "Mumbai" (unchanged after deep copy)
 // Cloned → "Banglore"
 
